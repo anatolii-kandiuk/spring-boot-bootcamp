2641bb6d8ec973d0654e95fb98582fff8db82608
anatolii <batkovich327@gmail.com>
2023-02-20 22:00:01 +0200

    BE - Start MVC Challenge Part2-3 Done

--- 02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
+++ 02. model-view-controller/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
@@ -2,4 +2,10 @@ package com.ltp.globalsuperstore;
 
 public class Constants {
     public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
+
+    public static final int NOT_FOUND = -10000;
+
+    public static final String SUCCESS_STATUS = "success";
+
+    public static final String FAILED_STATUS = "failed";
 }
